74b8774b748e02c696b1a855b0e6da04e2c6b0dc
Namrata Bhosale <namratambhosale2006@gmail.com>
2026-03-07 02:42:11 +0530

    Add FloraSense project with model

---
+++ backend/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+from model_loader import load_model
+
+app = Flask(__name__)
+CORS(app)
+
+model = load_model()
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+@app.route("/")
+def home():
+    return "Plant Detection API running"
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    if "image" not in request.files:
+        return jsonify({"error": "No image uploaded"})
+
+    file = request.files["image"]
+
+    img = Image.open(file).convert("RGB")
+    img = transform(img).unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(img)
+        prediction = torch.argmax(output, 1).item()
+
+    if prediction == 0:
+        result = "Invasive"
+    else:
+        result = "Non-Invasive"
+
+    return jsonify({
+        "plant_type": result
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
