13da56225765b1ccb9707033f2b33ede2daacfe4
Namrata1507 <namratambhosale2006@gmail.com>
2026-03-07 05:17:20 +0530

    Update model_loader.py

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -2,16 +2,13 @@ import torch
 import torchvision.models as models
 
 def load_model():
-
     model = models.resnet18(pretrained=False)
-
     model.fc = torch.nn.Linear(model.fc.in_features, 2)
 
-    model_path = "../models/invasive_model.pth"
+    model_path = "models/invasive_model.pth"   # fixed path
 
     state_dict = torch.load(model_path, map_location=torch.device("cpu"))
     model.load_state_dict(state_dict)
 
     model.eval()
-
     return model
